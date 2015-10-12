c46aee9554ff278d3a1defb09b182b9b7bc87977
cyxou <acyxou@gmail.com>
2015-10-12 16:53:30 +0300

    Skip empty lines in CSV

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ var Baby = require('babyparse');
 var fio = require('imacros-fio');
 
 module.exports = function(csvFileName) {
-  return Baby.parse(fio.read(csvFileName), { header: true }).data;
+  return Baby.parse(fio.read(csvFileName), {
+    header: true,
+    skipEmptyLines: true
+  }).data;
 };
